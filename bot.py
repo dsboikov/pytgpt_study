d11bc597b98c844b4f33a07b2a8006760e2d7f2f
Denis <dsboikov@yandex.ru>
2024-12-01 22:37:53 +0300

    Added resume creation functionality

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'gpt': 'Задать вопрос чату GPT 🤖',
         'talk': 'Поговорить с известной личностью 👤',
         'quiz': 'Поучаствовать в квизе ❓'
+        'help_with_resume': 'Помощь с резюме',
         # Добавить команду в меню можно так:
         # 'command': 'button text'
 
@@ -36,8 +37,50 @@ async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pass
 
+async def help_with_resume(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    dialog.mode = "resume_profile"
+    text = load_message("resume_profile")
+    await send_photo(update, context, "resume_profile")
+    await send_text(update, context, text)
+
+    dialog.user.clear()
+    dialog.count = 0
+    await send_text(update, context, "Введите название желаемой вакансии")
+
+
+async def help_with_resume_dialog(update, context):
+    text = update.message.text
+    dialog.count += 1
+
+    if dialog.count == 1:
+        dialog.user["vacancy"] = text
+        await send_text(update, context, "Введите ваши ФИО")
+    elif dialog.count == 2:
+        dialog.user["fio"] = text
+        await send_text(update, context, "Опыт работы: названия компаний, должности, обязанности")
+    elif dialog.count == 3:
+        dialog.user["experience"] = text
+        await send_text(update, context, "Какими навыками обладаете?")
+    elif dialog.count == 4:
+        dialog.user["skills"] = text
+        await send_text(update, context, "Добавьте информацию об образовании и квалификации")
+    elif dialog.count == 5:
+        dialog.user["qualification"] = text
+        prompt = load_prompt("resume_profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+        my_message = await send_text(update, context,
+                                     "ChatGPT занимается генерацией вашего резюме. Подождите пару секунд...")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+async def mode_handler(update, context):
+    if dialog.mode == "resume_profile":
+        await help_with_resume_dialog(update, context)
+
 
 ob_keys = Keys()
+dialog = Dialog()
+
 chat_gpt = ChatGptService(ob_keys.gpt_token)
 app = ApplicationBuilder().token(ob_keys.bot_token).build()
 app.add_handler(CommandHandler("start", start))
@@ -45,10 +88,12 @@ app.add_handler(CommandHandler("random", random))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("talk", talk))
 app.add_handler(CommandHandler("quiz", quiz))
+app.add_handler(CommandHandler("help_with_resume", help_with_resume))
 # Зарегистрировать обработчик команды можно так:
 # app.add_handler(CommandHandler('command', handler_func))
-
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, mode_handler))
 # Зарегистрировать обработчик коллбэка можно так:
 # app.add_handler(CallbackQueryHandler(app_button, pattern='^app_.*'))
+
 app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
