1b9e5346869827b0b1b80e877598bcb1d0629267
Denis <dsboikov@yandex.ru>
2024-12-01 12:55:25 +0300

    Added empty methods

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes
-
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
+from credentials import Keys
 from gpt import ChatGptService
-from util import (load_message, send_text, send_image, show_main_menu,
-                  default_callback_handler)
+from util import (load_message, send_text, send_image, show_main_menu, default_callback_handler)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -22,9 +21,30 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     })
 
 
-chat_gpt = ChatGptService('ChatGPT TOKEN')
-app = ApplicationBuilder().token('Telegram TOKEN').build()
+async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
 
+
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+ob_keys = Keys()
+chat_gpt = ChatGptService(ob_keys.gpt_token)
+app = ApplicationBuilder().token(ob_keys.bot_token).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("random", random))
+app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("talk", talk))
+app.add_handler(CommandHandler("quiz", quiz))
 # Зарегистрировать обработчик команды можно так:
 # app.add_handler(CommandHandler('command', handler_func))
 
